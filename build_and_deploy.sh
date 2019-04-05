8056230bba53cebca28c964d456d76ccb1936291
jakew <jakew@remitly.com>
2019-04-04 20:40:37 -0700

    fixing deploy script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,4 +1,4 @@
 docker build -t comparer_app .
 eval $(aws ecr get-login --no-include-email --profile personal | sed 's|https://||')
-docker tag comparer_app:latest 563257909816.dkr.ecr.us-west-2.amazonaws.com/comparer_app20190323
-docker push 563257909816.dkr.ecr.us-west-2.amazonaws.com/comparer_app20190323
+docker tag comparer_app:latest $ECR_REG_ADDRESS
+docker push $ECR_REG_ADDRESS
